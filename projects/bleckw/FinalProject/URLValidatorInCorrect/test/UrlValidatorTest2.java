0000fac3548f470103bc5897fb579f1d294740b3
Jordan Bleck <jordanbleck@gmail.com>
2019-08-08 01:42:50 +0000

    Add files via upload

---
+++ projects/bleckw/FinalProject/URLValidatorInCorrect/test/UrlValidatorTest2.java
@@ -0,0 +1,36 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import junit.framework.TestCase;
+
+/**
+ * Performs Validation Test for url validations.
+ *
+ * @version $Revision$
+ */
+public class UrlValidatorTest2 extends TestCase {
+
+   public UrlValidatorTest2(String testName) {
+      super(testName);
+   }
+
+   public void testUnitTest() {
+      String[] schemes = {"http","https"};
+	  UrlValidator urlVal = new UrlValidator(schemes);
+      assertTrue(urlVal.isValid("http://www.google.com"));
+   }
+}
